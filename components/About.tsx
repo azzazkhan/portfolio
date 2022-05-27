179817754f8fc22bc30cacd62746399a5576f0b6
Azzaz Khan <25636920+azzazkhan@users.noreply.github.com>
2022-05-27 18:18:23 +0000

    Updated about summary

--- components/About.tsx
+++ components/About.tsx
@@ -10,10 +10,10 @@ const AboutMe: FC = () => {
         Hi, I&apos;m Azzaz. Nice to meet you.
       </h1>
       <p className={styles.description}>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut accusamus rerum ab, adipisci,
-        harum aperiam quae nobis temporibus maiores quos saepe debitis ipsum? Fuga eos quaerat vitae
-        error, atque expedita et totam consectetur illum velit quo suscipit, doloremque odio
-        doloribus repellendus aliquam nisi dolores quidem laudantium tenetur aspernatur nesciunt.
+        Since beginning my journey as a freelancer designer nearly 4 years ago, I’ve done remote
+        work for agencies, consulted for startups, and collaborated with talented people to create
+        digital products for both business and consumer use. I’m quietly, naturally curious, and
+        perpetually working on improving my chops one challenge at a time.
       </p>
     </div>
   );
